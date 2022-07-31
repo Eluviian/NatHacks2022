682126b7967f466a97e2acb5b57a1667afc0f9f8
Eluviian <bayleysapara@gmail.com>
2022-07-30 19:24:20 -0600

    enemy chases player, does not change rooms yet

---
+++ Bayley - Game Related Files/EnemyCharacter.py
@@ -0,0 +1,105 @@
+import arcade
+from global_vars import *
+
+class EnemyCharacter(arcade.Sprite):
+    def __init__(self):
+
+        # Set up parent class
+        super().__init__()
+
+        # Default to face-right
+        self.character_face_direction = DOWN_FACING
+        self.room = 99
+        self.chasing_player = False
+
+        # Used for flipping between image sequences
+        self.cur_texture = 0
+
+        self.scale = CHARACTER_SCALING
+        
+        main_path = "Nurse"
+
+        # Load textures for idle standing
+        self.idle_textures = []
+        for direction in ["forward","left","right","back"]:
+            self.idle_textures.append(f"{main_path}/{direction}0.png")
+        
+        #forward, left, right, back
+        
+        # Load textures for walking
+        self.walk_textures = []
+        for direction in ["forward","left","right","back"]:
+            for i in range(4):
+                texture = f"{main_path}/{direction}{i}.png"
+                self.walk_textures.append(texture)
+
+                   
+            
+            
+
+    def update_animation1(self, delta_time: float = 1 / 60):
+        # Figure out if we need to flip face left or right
+        if self.change_x < 0: #and self.character_face_direction == RIGHT_FACING:
+            self.character_face_direction = LEFT_FACING
+        elif self.change_x > 0: #and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
+          
+        elif self.change_y < 0:
+            self.character_face_direction = DOWN_FACING
+        elif self.change_y > 0:
+            self.character_face_direction = UP_FACING           
+
+                
+        
+            
+        # Idle animation
+        if self.change_x == 0 and self.change_y == 0:
+            self.texture = arcade.load_texture(self.idle_textures[self.character_face_direction])
+            
+
+        # Walking animation
+        self.cur_texture += 1
+        if self.cur_texture > 3 * UPDATES_PER_FRAME:
+            self.cur_texture = 0
+        frame = self.cur_texture // UPDATES_PER_FRAME
+        direction = self.character_face_direction
+        self.texture = arcade.load_texture(self.walk_textures[4*direction + frame])
+        
+        
+    def transfer_room(self, starting_room, goal_room):
+        if starting_room == 0:
+            if goal_room == 1:
+                pass
+            if goal_room == 2:
+                pass
+            if goal_room == 3:
+                pass
+        if starting_room == 1:
+            if goal_room == 0:
+                pass
+            if goal_room == 2:
+                pass
+        if starting_room == 2:
+            if goal_room == 0:
+                pass
+            if goal_room == 1:
+                pass
+            if goal_room == 3:
+                pass
+        if starting_room == 3:
+            if goal_room == 0:
+                pass
+            if goal_room == 2:
+                pass
+            if goal_room == 7:
+                pass
+        if starting_room == 7:
+            if goal_room == 3:
+                pass
+            if goal_room == 8:
+                pass
+        if starting_room == 8:
+            if goal_room == 7:
+                pass
+        
+        
---
+++ Bayley - Game Related Files/MyGame.py
@@ -0,0 +1,172 @@
+"""
+Move with a Sprite Animation
+
+Simple program to show basic sprite usage.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_move_animation
+"""
+import arcade
+import random
+from PlayerCharacter import PlayerCharacter
+from EnemyCharacter import EnemyCharacter
+from global_vars import *
+
+
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height, title):
+        """ Set up the game and initialize the variables. """
+        super().__init__(width, height, title)
+
+        # Set up the player
+        self.player = None
+        self.enemy = None
+
+    def setup(self):
+        
+        # Set up the player
+        self.enemy_timer = 0
+        self.enemy_spawned = False
+        
+        self.player = PlayerCharacter()
+        self.player.center_x = 250
+        self.player.center_y = 250
+        self.player.scale = CHARACTER_SCALING
+        
+        self.enemy = EnemyCharacter()
+        self.enemy.center_x = 300
+        self.enemy.center_y = 200
+        self.enemy.scale = ENEMY_SCALING        
+        
+
+        self.background = arcade.load_texture("Maps\exit_room.png")
+        self.room = 3
+        
+        
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        self.clear()
+        
+        arcade.start_render()
+        arcade.draw_texture_rectangle(300,300,600,600,self.background)  
+        
+        #arcade.draw_lrtb_rectangle_filled(5,595,260,255,arcade.color.BLUSH)
+        #arcade.draw_lrtb_rectangle_filled(5,595,150,145,arcade.color.BLUSH)
+
+        # Draw all the sprites.
+        self.player.draw()
+        if self.enemy.room == self.room:
+            self.enemy.draw()
+        
+        #arcade.draw_lrtb_rectangle_filled(178,204,272,260,arcade.color.BLUSH)
+        #arcade.draw_lrtb_rectangle_filled(387,427,140,100,arcade.color.BLUSH)
+        #arcade.draw_lrtb_rectangle_filled(176,202,272,260,arcade.color.BLUSH)
+        
+        
+        
+
+    def on_key_press(self, key, modifiers):
+        """
+        Called whenever a key is pressed.
+        """
+        if key == arcade.key.UP:
+            self.player.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """
+        Called when the user releases a key.
+        """
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player.change_x = 0
+            
+                
+                
+        
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player
+        self.player.update()
+        self.enemy.update()
+        
+        prev_room = self.room
+
+        # Update the players animation
+        self.room = self.player.update_animation1(self.room)
+        self.enemy.update_animation1()
+        
+        
+        if self.enemy_timer == 200:
+            
+            if self.room == 0 or self.room == 1 or self.room == 2:
+                self.enemy.room = 8
+                #print("enemy is in room",self.enemy.room)
+            if self.room == 3 or self.room == 7 or self.room == 8:
+                self.enemy.room = 1
+                #print("enemy is in room "+str(self.enemy.room))
+                
+          
+        if self.enemy.room == self.room:
+            self.chase_timer = 0
+            self.enemy.chasing_player = True
+            
+        if self.enemy.chasing_player == True:
+            if self.enemy.room == self.room:
+                self.enemy_pursue()
+            '''
+            elif self.enemy_timer == self.chase_timer + 50:
+                self.enemy.transfer_room(self.enemy.room,self.room)'''
+            
+        
+        
+        if self.room == 0:
+            map_string = "Maps/exit_room.png"
+
+        else:
+            map_string = f"Maps/room{self.room}.png"
+           
+        
+        self.enemy_timer += 1
+        self.background = arcade.load_texture(map_string)
+        
+
+    def enemy_pursue(self):
+        if self.player.center_x < self.enemy.center_x:
+            self.enemy.change_x = -ENEMY_MOVEMENT_SPEED
+        else:
+            self.enemy.change_x = ENEMY_MOVEMENT_SPEED
+            
+        if self.player.center_y < self.enemy.center_y:
+            self.enemy.change_y = -ENEMY_MOVEMENT_SPEED
+        
+        else:
+            self.enemy.change_y = ENEMY_MOVEMENT_SPEED       
+            
+        
+
+def main():
+    """ Main function """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
